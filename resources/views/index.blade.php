8c8bd3472dae913058e4af3b8f33791ab3afd835
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-12-17 22:22:47 +0630

    app layout create, docker mysql run and view changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,15 @@
-<h1>List of Tasks</h1>
+@extends('layouts.app')
 
+@section('title', 'List of Tasks')
 
-@forelse ($tasks as $task)
-    <p>
-        <a href={{ route('task.show', ['id' => $task->id]) }}>
-            {{ $task->title }}
-        </a>
-    </p>
-@empty
-    <p>There are no tasks!</p>
-@endforelse
+@section('content')
+    @forelse ($tasks as $task)
+        <p>
+            <a href={{ route('task.show', ['id' => $task->id]) }}>
+                {{ $task->title }}
+            </a>
+        </p>
+    @empty
+        <p>There are no tasks!</p>
+    @endforelse
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 class Task
@@ -54,13 +55,24 @@ $tasks = [
     ),
 ];
 
+Route::get('/', function () {
+    return redirect()->route('task.index');
+});
 
-Route::get('/', function () use ($tasks) {
+Route::get('/tasks', function () use ($tasks) {
     return view('index', [
         'tasks' => $tasks
     ]);
 })->name('task.index');
 
-Route::get('/{id}', function () {
-    return 'one single task';
+Route::get('/tasks/{id}', function ($id) use ($tasks) {
+    $task = collect($tasks)->firstWhere('id', $id);
+
+    if (!$task) {
+        abort(Response::HTTP_NOT_FOUND);
+    }
+
+    return view('show', [
+        'task' => $task
+    ]);
 })->name('task.show');

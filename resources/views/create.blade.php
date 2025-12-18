b7b65e04d8be609397a45e00c7d1c51534d4c09c
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-12-19 01:33:59 +0630

    old value are remained if the create form failed

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -16,21 +16,21 @@
         @csrf
         <div>
             <label for="title">Title:</label>
-            <input type="text" id="title" name="title">
+            <input type="text" id="title" name="title" value="{{ old('title') }}">
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
         <div>
             <label for="description">Description:</label>
-            <textarea id="description" name="description" rows="5"></textarea>
+            <textarea id="description" name="description" rows="5">{{ old('description') }}</textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
         <div>
             <label for="long_description">Long Description:</label>
-            <textarea id="long_description" name="long_description" rows="10"></textarea>
+            <textarea id="long_description" name="long_description" rows="10">{{ old('long_description') }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
